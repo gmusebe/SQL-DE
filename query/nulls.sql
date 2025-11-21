e4eef6059d74a4a6899c91d69cde34dd49522fa1
Gmusebe <musebeivan@gmail.com>
2025-11-21 13:29:08 +0300

    IS NULL | IS NOT NULL | NULL DATA POLICIES

--- query/nulls.sql
+++ query/nulls.sql
@@ -54,7 +54,7 @@ FROM sales.Customers
 ORDER BY CASE WHEN Score IS NULL THEN 1 ELSE 0  END, Score
 
 -- Value to NULL
--- NULLIF - Accepts two values
+-- NULLIF** - Accepts two values
 -- Use Case: Prevention the error of dividing by zero
 SELECT 
  OrderID,
@@ -62,4 +62,50 @@ SELECT
  Sales,
  Quantity,
  Sales/ NULLIF(Quantity,0) AS Price
-FROM sales.Orders
+FROM sales.Orders;
+
+-- Check Nulls
+-- IS NULL
+-- IS NOT NULL
+-- Use Case
+USE MyDatabase;
+SELECT
+*
+FROM customers
+WHERE score IS NULL
+
+SELECT
+*
+FROM customers
+WHERE score IS NOT NULL
+
+-- ANTI JOIN
+USE SalesDB;
+
+SELECT
+a.*,
+b.OrderID
+FROM Sales.Customers a
+LEFT JOIN Sales.Orders b
+ON a.CustomerID = b.CustomerID
+WHERE b.CustomerID IS NULL
+
+
+-- Data Policy
+WITH Orders AS (
+	SELECT 1 Id, 'A' Category UNION
+	SELECT 2, NULL UNION
+	SELECT 3,  '' UNION
+	SELECT 4, '  '
+)
+SELECT
+	*,
+	DATALENGTH(Category) CategoryLen,
+	-- Policy 1: Only Use NULLS & Empty Strings ''
+	TRIM(Category) AS Policy1,
+	DATALENGTH(TRIM(Category)) Policy1Len,
+	-- Policy 2: Only use NULLS; NULLIF - If Equal Returns NULL | If Not Equal Returns First Value = NULL
+	NULLIF(TRIM(Category), '') Policy2,
+	-- Policy 3: For Visualisation, Known Value
+	COALESCE(NULLIF(TRIM(Category), ''), 'UNKNOWN') Policy3
+FROM Orders;
