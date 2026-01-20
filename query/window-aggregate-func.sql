178eb91dedff8b12d226d272fbcad5a01f4687a5
Gmusebe <musebeivan@gmail.com>
2026-01-20 08:43:01 +0300

    COUNT, SUM, AVG, MIN, MAX

--- query/window-aggregate-func.sql
+++ query/window-aggregate-func.sql
@@ -1,13 +1,103 @@
--- 1. COUNT
--- How many orders do we have of each product:
+-- COUNT
+-- Allows all data types
+-- Count the Number of Rows in our Table:
 
+-- #1. OVERALL ANALYSIS
+-- Find The Total No. of Order
 SELECT
-    ProductID,
-    COUNT(ProductID) OVER(PARTITION BY ProductID)
-FROM sales.orders
+	COUNT(*) TotalOrders
+FROM Sales.Orders;
 
--- Allows any datatype:
--- Find Total number of ord Counters
+-- Provide details of Order ID and Date:
 SELECT
-    COUNT(ProductID) OVER(PARTITION BY ProductID)
-FROM sales.orders
+	OrderID,
+	OrderDate,
+	COUNT(*) OVER() TotalOrders
+FROM Sales.Orders;
+
+
+-- #2. CATEGORY ANALYSIS / #3. QUALITY CHECK NULLS
+-- Find the Total Number of Orders for each Customer
+SELECT
+	CustomerID,
+	COUNT(*) OVER(PARTITION BY CustomerID) [TotalOrders Per Customer]
+FROM Sales.Orders;
+
+-- Find the Total No. of Customers, Provide All Customer Details
+SELECT
+	*,
+	COUNT(*) OVER() TotalCustomers
+FROM Sales.Customers;
+
+-- #3. IDENTIFY DUPLICATES
+-- Find Total No. of Scores for customers
+SELECT
+	CustomerID,
+	COUNT(Score) OVER() ScoreCount
+FROM
+	Sales.Customers;
+
+-- COUNT to Identify Duplicates
+SELECT *
+FROM(
+	SELECT
+		OrderID,
+		COUNT(*) OVER(PARTITION BY OrderID) CheckPK
+	FROM Sales.OrdersArchive
+) t
+WHERE CheckPK > 1
+
+
+-- SUM
+-- Total Sales for all orders & For Each Product
+-- #1. OVERALL ANALYSIS
+SELECT
+	OrderID,
+	OrderDate,
+	ProductID,
+	SUM(Sales) OVER() TotalSales,
+	SUM(Sales) OVER(PARTITION BY ProductID) TotalSalesByProduct
+FROM Sales.Orders;
+
+SELECT *
+FROM Sales.Orders
+
+-- #2. COMPARISON ANALYSIS
+-- Find % Contibution of Each Product's Sales to Total Sales
+-- PART TO WHOLE ANALYSIS
+SELECT
+	OrderID,
+	ProductID,
+	Sales,
+	SUM(Sales) OVER() TotalSales,
+	ROUND(CAST (Sales AS Float)/SUM(Sales) OVER() * 100, 2) [%ofTotal]
+
+FROM Sales.Orders;
+
+
+-- AVERAGE
+SELECT
+	OrderID,
+	ProductID,
+	AVG(Sales) OVER() AvgSales,
+	AVG(Sales) OVER (PARTITION BY ProductID) AvgSalesByProduct
+FROM Sales.Orders;
+
+-- Average Scores of Customers
+SELECT
+	AVG(COALESCE( Score, 0)) OVER() AvgScore
+FROM Sales.Customers
+
+SELECT *
+FROM(
+	SELECT
+		OrderID,
+		ProductID,
+		Sales,
+		AVG(Sales) OVER() AvgSales
+	FROM Sales.Orders
+)t
+WHERE Sales > AvgSales
+
+
+-- #RUNNING & ROLLING TOTAL 
