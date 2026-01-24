563913f24ea50384960571c69d3f2fa42318640a
Gmusebe <musebeivan@gmail.com>
2026-01-24 15:33:39 +0300

    Standalone | Nested CTEs

---
+++ query/CTEs.sql
@@ -0,0 +1,58 @@
+-- Standalone CTEs
+-- Used Independently in the query
+WITH CTE_TotalSales AS
+	-- #Step 1: Find the Total Sales per Customer
+	(SELECT
+		 CustomerID,
+		 SUM(Sales) TotalSales
+	FROM Sales.Orders
+	GROUP BY CustomerID
+	),
+	-- #Step 2: Find the Last Order of each Customer
+	-- Multiple CTEs
+	CTE_LastOrder AS
+	(SELECT 
+		CustomerID,
+		MAX(OrderDate) LastOrder
+	FROM Sales.Orders
+	GROUP BY CustomerID
+	),
+	-- #Step 3: Rank Customers based on Total Sales Per Customer
+	CTE_CustomerRank AS
+	(SELECT 
+		CustomerID,
+		TotalSales,
+		RANK() OVER(ORDER BY TotalSales DESC) CustomerRank
+	FROM CTE_TotalSales
+	),
+	-- #Step 4: Segment Customers based on their Total Sales
+	CTE_CustomerSegments AS
+	(SELECT 
+		CustomerID,
+		CASE
+			WHEN TotalSales > 100 THEN 'High'
+			WHEN TotalSales > 50 THEN 'Medium'
+			ELSE 'Low'
+		END CustomerSegments
+	FROM CTE_TotalSales
+	)
+
+
+-- Main Query
+SELECT
+	c.CustomerID,
+	c.FirstName,
+	c.LastName,
+	cts.TotalSales,
+	clo.LastOrder,
+	ccr.CustomerRank,
+	ccs.CustomerSegments
+FROM Sales.Customers c
+LEFT JOIN CTE_TotalSales cts
+ON cts.CustomerID =c.CustomerID
+LEFT JOIN CTE_LastOrder clo
+ON clo.CustomerID =c.CustomerID
+LEFT JOIN CTE_CustomerRank ccr
+ON ccr.CustomerID =c.CustomerID
+LEFT JOIN CTE_CustomerSegments ccs
+ON ccs.CustomerID =c.CustomerID
