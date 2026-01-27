3268ee2517c992a1bd2a457bbedf1775e0b785d8
Gmusebe <musebeivan@gmail.com>
2026-01-27 23:06:11 +0300

    Stored Procedures

--- query/stored_procedures.sql
+++ query/stored_procedures.sql
@@ -9,7 +9,7 @@ FROM Sales.Customers
 WHERE Country = 'USA';
 
 -- Turn Query into a stored procedure:
-/*...
+GO
 CREATE PROCEDURE GetCustomerSummary AS
 BEGIN
 
@@ -23,13 +23,12 @@ END;
 
 -- Execute the Stored Procedure
 EXEC GetCustomerSummary;
-...*/
 
 
 -- PARAMETERS
 -- For German Customers  Find the total number of customers and the average score:
-/*
-CREATE PROCEDURE GetCustomerSummary AS
+GO
+ALTER PROCEDURE GetCustomerSummaryGermany AS
 BEGIN
 SELECT
     COUNT(*) TotalCustomers,
@@ -40,11 +39,11 @@ END
 
 -- Execute the Stored Procedure
 EXEC GetCustomerSummary;
-...*/
+
 
 -- PARAMETERS
 -- For German Customers  Find the total number of customers and the average score:
-/*
+GO
 ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) AS
 BEGIN
 SELECT
@@ -53,19 +52,20 @@ SELECT
 FROM Sales.Customers
 WHERE Country = @Country
 END
-*/
+
 
 -- Execute the Stored Procedure
-/* EXEC GetCustomerSummary @Country = 'Germany'; */
+EXEC GetCustomerSummary @Country = 'Germany';
 
-/*  EXEC GetCustomerSummary @Country = 'USA'; */
+EXEC GetCustomerSummary @Country = 'USA';
 
 
 -- DROP
+GO
 DROP PROCEDURE GetCustomerSummaryGermany;
 
 -- Setting Default value
-/*
+GO
 ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA'
 AS 
 BEGIN
@@ -77,7 +77,7 @@ WHERE Country = @Country
 END
 
 EXEC GetCustomerSummary @Country  = 'USA';
-*/
+
 
 -- Multi Queries in Stored Procedures
 --  Find the total Nr. of Orders  and Total Sales
@@ -90,11 +90,11 @@ ON c.CustomerID = o.CustomerID
 WHERE C.Country = 'USA'
 
 
-
+-- Variables
+GO
 ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA'
 AS 
 BEGIN
-
 DECLARE @TotalCustomers INT, @AvgScore FLOAT;
 
     SELECT
@@ -120,4 +120,64 @@ END
 EXEC GetCustomerSummary @Country = 'Germany'
 
 
--- Variables
+
+-- CONTROL FLOW: IF ELSE
+GO
+ALTER PROCEDURE GetCustomerSummary @Country NVARCHAR(50) = 'USA'
+AS 
+BEGIN
+
+    BEGIN TRY
+        DECLARE @TotalCustomers INT, @AvgScore FLOAT;
+            --Prepare & Clean Up Data [nulls in scores]
+            
+            IF EXISTS(SELECT 1 FROM Sales.Customers WHERE SCORE IS NULL AND Country = @Country)
+            BEGIN
+                PRINT('Updating NULL scores to 0');
+                UPDATE Sales.Customers
+                SET Score = 0
+                WHERE Score IS NULL AND Country = @Country;
+            END
+
+            ELSE
+            BEGIN
+                PRINT('No NULL scores found');
+            END;
+
+            -- Generating Reports 
+            SELECT
+                @TotalCustomers = COUNT(*),
+                @AvgScore = AVG(Score)
+            FROM Sales.Customers
+            WHERE Country = @Country;
+
+            PRINT 'Total Customers from' + @Country + ':' + CAST(@TotalCustomers AS NVARCHAR);
+            PRINT 'Average Score from' + @Country + ':' + CAST(@AvgScore AS NVARCHAR) ;
+
+            --  Find the total Nr. of Orders  and Total Sales
+            SELECT
+                COUNT(OrderID) TotalOrders,
+                SUM(Sales) TotalSales,
+                1/0
+            FROM Sales.Orders o
+            JOIN Sales.Customers c
+            ON c.CustomerID = o.CustomerID
+            WHERE C.Country = @Country;
+
+    END TRY
+    -- Error Handling & TRY CATCH
+    BEGIN CATCH
+        PRINT('An error occured');
+        PRINT('Error Message: ' + ERROR_MESSAGE());
+        PRINT('Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR));
+        PRINT('Error Line: ' + CAST(ERROR_LINE() AS VARCHAR));
+        PRINT('Error procedure: ' + ERROR_PROCEDURE());
+    END CATCH
+END
+
+
+EXEC GetCustomerSummary @Country = 'Germany'
+
+
+
+-- Styling Stored Procedures
