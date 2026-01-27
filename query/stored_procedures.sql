813dc4b8c61cd22eb942d6243b32b04a3a029b6a
Gmusebe <musebeivan@gmail.com>
2026-01-27 23:41:13 +0300

    Triggers

--- query/stored_procedures.sql
+++ query/stored_procedures.sql
@@ -176,8 +176,34 @@ BEGIN
 END
 
 
-EXEC GetCustomerSummary @Country = 'Germany'
+EXEC GetCustomerSummary @Country = 'Germany';
 
 
 
--- Styling Stored Procedures
+-- Triggers
+CREATE TABLE Sales.EmployeeLogs (
+    LogID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeID INT,
+    LogMessage VARCHAR(255),
+    LogDate DATETIME
+);
+
+GO
+CREATE TRIGGER trg_AfterInsertEmployee ON Sales.Employees
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO Sales.EmployeeLogs(EmployeeID, LogMessage, LogDate)
+    SELECT 
+        EmployeeID,
+        'New Employee Added = ' + CAST(EmployeeID AS VARCHAR),
+        GETDATE()
+    FROM INSERTED
+END;
+
+
+SELECT * FROM Sales.EmployeeLogs
+
+INSERT INTO Sales.Employees
+VALUES
+    (7, 'Maria', 'Doe', 'HR', '1988-01-12', 'F', 80000, 3)
