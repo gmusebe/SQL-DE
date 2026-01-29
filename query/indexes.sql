85155b06fd031355675e1a14986ac33e487cb151
Gmusebe <musebeivan@gmail.com>
2026-01-29 23:54:45 +0300

    CLUSTERED \ NONCLUSTERED INDEX

--- query/indexes.sql
+++ query/indexes.sql
@@ -4,7 +4,35 @@
 USE SalesDB;
 
 -- SYNTAX
-CREATE CLUSTERED INDEX...
+-- CREATE THE INDEX
+-- CREATE [CLUSTERED | NONCLUSTERED] INDEX index_name ON table_name (col1, col2) ...
+
+SELECT *
+INTO Sales.DBCustomers
+FROM Sales.Customers;
+
+SELECT *
+FROM Sales.Customers
+WHERE CustomerID = 1;
+
+
+CREATE CLUSTERED INDEX idx_DBCustomers_CustomerID
+ON Sales.DBCustomers(CustomerID);
+
+CREATE CLUSTERED INDEX idx_DBCustomers_FirstName
+ON Sales.DBCustomers(FirstName);
+
+--DROP INDEX
+DROP INDEX idx_DBCustomers_CustomerID ON Sales.DBCustomers;
+
+-- EXAMPLE 2
+SELECT *
+FROM Sales.DBCustomers
+WHERE LastName = 'Brown';
+
+-- CREATE NON-CLUSTERED INDEX
+CREATE NONCLUSTERED INDEX idx_DBCustomers_LastName
+ON Sales.DBCustomers(LastName);
 
 -- STORAGE INDEX
 -- 1. ROWSTORE INDEX
